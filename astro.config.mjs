be1c62a139971fb1b056ee375b20cc2f1260ec49
Giridharan <ggiridharan097@gmail.com>
2025-12-01 22:03:54 +0530

    Rebuild site with correct config and .nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mittai17.github.io/simpleaiguide',
-  base: '/simpleaiguide/',
+  site: 'https://mittai17.github.io',
+  base: '/simpleaiguide',
   integrations: [tailwind()],
   outDir: 'docs',
   build: {

e50a7ea00eafd7d17897bb6ffbb7f583c2dfa58e
Giridharan <ggiridharan097@gmail.com>
2025-12-01 19:41:50 +0530

    Configure base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://simpleaiguide.tech',
+  site: 'https://mittai17.github.io',
+  base: '/simpleaiguide/',
   integrations: [tailwind()],
 });

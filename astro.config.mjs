03a432c6b87eb8c9474bf09f4c876521b435df3f
Giridharan <ggiridharan097@gmail.com>
2025-12-02 23:20:44 +0530

    Configure external links to open in new tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://simpleaiguide.tech',
   integrations: [tailwind(), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+    ]
+  },
   outDir: 'docs',
   build: {
     outDir: 'docs',

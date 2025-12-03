a8df3863e0d59249bb9dc43b8a5285f098818bf4
Giridharan <ggiridharan097@gmail.com>
2025-12-03 10:55:10 +0530

    Improve Learn Sidebar with accordion style

--- docs/chunks/1-4-types-of-ai_CBZzJ-kh.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./1-4-types-of-ai_D6cWyNem.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
